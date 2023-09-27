a2a36353a6d1b3f082f97e314447a04ca200d2fc
Boddudinesh <115147974+Boddudinesh@users.noreply.github.com>
2023-09-27 16:44:26 +0530

    Update t1.py

--- t1.py
+++ t1.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import js
 import matplotlib.pyplot as plt
-fig, ax = plt.subplot()
+
 r=js.localStorage.getItem('myValue')
 from pyodide.http import open_url
 url = 'https://raw.githubusercontent.com/Boddudinesh/automation/main/SEM-3.2.csv'
@@ -14,11 +14,6 @@ l2 = list(d[0].values())[4:]
 l3=[]
 for i in l2:
     l3.append(int(i.split()[0]))
-bars=plt.bar(l1 , l3)
-for bar in bars:
-    yval = bar.get_height()
-    plt.text(bar.get_x() + bar.get_width()/2, yval, round(yval, 2), ha='center', va='bottom')
-plt.xlabel('Year')
-plt.ylabel('Population (M)')
-plt.title('Year vs Population')
+fig,ax=plt.bar(l1 , l3)
+
 fig
